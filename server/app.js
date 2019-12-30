6f4c94d830624c703191b1187d8cd73bd4dc8fdc
saran <saran.babu@infosys.com>
2019-12-30 17:25:30 +0530

    api change
    Signed-off-by: saran <saran.babu@infosys.com>

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ const port = 3000
 var child_process = require('child_process')
 
 app.get('/', (req, res) => {
-	child_process.exec('cd.. && cd.. && cd Hackathone && cd mailer && npm run build:email', function (error, stdout, stderr) {
+	child_process.exec('cd.. && cd.. && cd mailer-campaign && cd front_end && npm run build:email', function (error, stdout, stderr) {
 		console.log('build success!')
-		child_process.exec('cd.. && cd.. && cd Hackathone && cd mailer && generate-email.bat', function (error, stdout, stderr) {
+		child_process.exec('cd.. && cd.. && cd mailer-campaign && cd front_end && generate-email.bat', function (error, stdout, stderr) {
 			console.log('gen success!')
 			res.send('Email generated successfully!')
 		});
